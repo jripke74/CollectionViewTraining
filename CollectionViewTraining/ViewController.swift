6ea3c8233b8f0fc4295f43e6297144768426a316
Jeff Ripke <jripke74@gmail.com>
2018-02-18 14:20:24 -0800

    added cell selection

--- CollectionViewTraining/ViewController.swift
+++ CollectionViewTraining/ViewController.swift
@@ -34,4 +34,9 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         }
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let text = collectionData[indexPath.row]
+        print("Selected: \(text)")
+    }
 }
